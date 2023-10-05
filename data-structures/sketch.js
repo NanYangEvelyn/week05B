1d1e3afb14f0d413ab30789e92c37af7b42a3d3b
NanYangEvelyn <ny2187@nyu.edu>
2023-10-05 19:38:50 -0400

    finish

--- data-structures/sketch.js
+++ data-structures/sketch.js
@@ -1,27 +1,37 @@
-let xPos = 0;
-let yPos = 150;
+let xPos = [];
+let yPos = [];
+let xVel = [];
+let cDiam = []; 
 
-let xVel = 2;
+let numCircle = 10;
 
-let cDiam = 40;
 
 function setup() {
   createCanvas(800, 600);
+  for(i = 0; i < numCircle; i++){
+xPos.push(0);
+yPos.push(random(30,height-30));
+xVel.push(random(2,5));
+cDiam.push(random(20,70));
+
+  }
 }
 
 function draw() {
   background("lightblue");
   fill(220, 10, 100);
 
-  // draw circle
-  ellipse(xPos, yPos, cDiam, cDiam);
 
-  // update pos
-  xPos = xPos + xVel;
+for(let i = 0; i < numCircle; i += 1){
+  ellipse(xPos[i], yPos[i], cDiam[i], cDiam[i]);
+  xPos[i] = xPos[i] + xVel[i];
 
-  // if reset is needed: pick random diameter and reset x
-  if (xPos > width + cDiam) {
-    cDiam = random(20, 75);
-    xPos = 0 - cDiam;
+  if (xPos[i] > width + cDiam[i]) {
+    cDiam[i] = random(20, 75);
+    xPos[i] = 0 - cDiam[i];
   }
+}
+
+
+
 }
